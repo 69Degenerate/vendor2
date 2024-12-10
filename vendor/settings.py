c3da5b90a83a8116cdba0e2a0a0d65ffe253bf63
69Degenerate <kaneki.kenki.1999@gmail.com>
2024-12-10 21:55:08 +0530

    truster origin

--- vendor/settings.py
+++ vendor/settings.py
@@ -123,3 +123,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+allowed_origins = [
+    "http://0.0.0.1:8000",
+    "https://vendor2-esfv.onrender.com"
+]
+
+CORS_ORIGIN_ALLOW = True
+CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = allowed_origins
+CORS_ORIGIN_WHITELIST = allowed_origins
+CORS_ALLOWED_ORIGINS = allowed_origins
+CSRF_TRUSTED_ORIGINS = allowed_origins
